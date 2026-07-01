7782def1340ff1a7715822e3f3de0deabd9116a3
Dragan Rapić <dragan.rapic@croders.com>
2026-07-01 12:21:17 +0200

    [web-shop] favicon

--- web-shop/src/app/layout.js
+++ web-shop/src/app/layout.js
@@ -9,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
